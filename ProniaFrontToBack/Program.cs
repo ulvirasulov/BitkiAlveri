f5c3eaea4b36853f453e68a9cb5a45f1bd00c8d1
ulvirasulov <ulvirr-ab107@code.edu.az>
2024-12-11 23:04:37 +0400

    updated

---
+++ ProniaFrontToBack/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProniaFrontToBack.Abstractions.EmailService;
+using ProniaFrontToBack.DAL;
+using ProniaFrontToBack.Helpers.Email;
+using ProniaFrontToBack.Models;
+using ProniaFrontToBack.Services;
+using System.Configuration;
+
+namespace ProniaFrontToBack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSession();
+
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+            builder.Services.AddTransient<IMailService, MailService>();
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._";
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequiredLength = 8;
+                opt.Lockout.AllowedForNewUsers = true;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+                opt.Lockout.MaxFailedAccessAttempts = 3;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("MsSql"));
+            });
+
+            var app = builder.Build();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSession();
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=dashboard}/{action=index}/{id?}"
+             );
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=index}/{id?}"
+             );
+            app.UseStaticFiles();
+            app.Run();
+        }
+    }
+}
